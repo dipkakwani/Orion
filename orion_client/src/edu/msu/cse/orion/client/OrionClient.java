58a95dba58ef79a9b701dbe69cc8223432be12cf
Diptanshu Kakwani <dipkakwani@gmail.com>
2020-04-09 00:00:42 +0530

    refactor code

--- orion_client/src/edu/msu/cse/orion/client/OrionClient.java
+++ orion_client/src/edu/msu/cse/orion/client/OrionClient.java
@@ -1,7 +1,8 @@
-package edu.msu.cse.causalSpartan.client;
+package edu.msu.cse.orion.client;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.util.logging.Level;
 
 import com.google.protobuf.ByteString;
 
@@ -16,19 +17,26 @@ import edu.msu.cse.dkvf.metadata.Metadata.GetMessage;
 import edu.msu.cse.dkvf.metadata.Metadata.PutMessage;
 import edu.msu.cse.dkvf.metadata.Metadata.RotMessage;
 
-public class CausalSpartanClient extends DKVFClient {
+public class OrionClient extends DKVFClient {
 
     List<Long> dsv;
     HashMap<Integer, Long> ds;
     int dcId;
     int numOfPartitions;
     int numOfDatacenters;
+    long heartRate;
+    long latency;          // Latency inter DC
+    long dsvComutationInterval;
+    long lastRotTime = 0;
 
-    public CausalSpartanClient(ConfigReader cnfReader) {
+    public OrionClient(ConfigReader cnfReader) {
         super(cnfReader);
         HashMap<String, List<String>> protocolProperties = cnfReader.getProtocolProperties();
         numOfDatacenters = new Integer(protocolProperties.get("num_of_datacenters").get(0));
         numOfPartitions = new Integer(protocolProperties.get("num_of_partitions").get(0));
+        heartRate = new Integer(protocolProperties.get("heartbeat_interval").get(0));
+        latency = new Integer(protocolProperties.get("max_latency").get(0));
+        dsvComutationInterval = new Integer(protocolProperties.get("dsv_comutation_interval").get(0));
 
         dcId = new Integer(protocolProperties.get("dc_id").get(0));
 
@@ -88,35 +96,73 @@ public class CausalSpartanClient extends DKVFClient {
     }
 
     public Map<String, ByteString> rot(Set<String> keys) {
-        try {
-            RotMessage rm = RotMessage.newBuilder().addAllKeys(keys)
-                            .addAllDsvItem(dsv).putAllDsItems(ds).build();
-            ClientMessage cm = ClientMessage.newBuilder().setRotMessage(rm).build();
+        long currentTime = edu.msu.cse.orion.client.Utils.getPhysicalTime();
+        long localOffset, remoteOffset;
+        if (lastRotTime != 0) {
+            localOffset = currentTime - heartRate - dsvComutationInterval - lastRotTime;
+            remoteOffset = currentTime - heartRate - dsvComutationInterval - latency - lastRotTime;
+        }
+        else
+            localOffset = remoteOffset = 0;
 
-            // Contact the partition of first key for ROT
-            int partition = findPartition(keys.iterator().next());
-            String serverId = dcId + "_" + partition;
-            if (sendToServer(serverId, cm) == NetworkStatus.FAILURE) {
-                protocolLOGGER.severe("Failed to send to server " + serverId);
-                return null;
+        if (localOffset < 0)
+            localOffset = 0;
+        if (remoteOffset < 0)
+            remoteOffset = 0;
+
+        long shiftedLocalOffset = edu.msu.cse.orion.client.Utils.shiftToHighBits(localOffset);
+        long shiftedRemoteOffset = edu.msu.cse.orion.client.Utils.shiftToHighBits(remoteOffset);
+
+        List<Long> predictedDSV = new ArrayList<>(dsv.size());
+
+        for (int i = 0; i < dsv.size(); i++) {
+            if (i != dcId)
+                predictedDSV.add(dsv.get(i) + shiftedRemoteOffset);
+            else
+                predictedDSV.add(dsv.get(i) + shiftedLocalOffset);
+        }
+
+        Map<String, ByteString> results = new HashMap<>(keys.size());
+        Map<String, List<String>> serverKeyMap = new HashMap<>();
+        try {
+            // Group keys with server
+            for (String key : keys) {
+                int partition = findPartition(key);
+                String serverId = dcId + "_" + partition;
+                serverKeyMap.computeIfAbsent(serverId, k -> new ArrayList<>()).add(key);
             }
-            ClientReply cr = readFromServer(serverId);
-            if (cr != null && cr.getStatus()) {
-                updateDsv(cr.getRotReply().getDsvItemList());
-                for (Map.Entry<Integer, Long> dsEntry : cr.getRotReply().getDsItemsMap().entrySet()) {
-                    updateDS(dsEntry.getKey(), dsEntry.getValue());
+            // Send ROT messages to servers
+            for (Map.Entry<String, List<String>> e : serverKeyMap.entrySet()) {
+                RotMessage rm = RotMessage.newBuilder().addAllDsvItem(predictedDSV).addAllKey(e.getValue()).build();
+                ClientMessage cm = ClientMessage.newBuilder().setRotMessage(rm).build();
+                if (sendToServer(e.getKey(), cm) == NetworkStatus.FAILURE) {
+                    protocolLOGGER.severe("Failed to send to server " + e.getKey());
+                    return null;
                 }
-                return cr.getRotReply().getKeyValueMap();
-            } else {
-                protocolLOGGER.severe("Server could not get the keys= " + keys);
-                return null;
+            }
+            lastRotTime = edu.msu.cse.orion.client.Utils.getPhysicalTime();
+            // Read replies from servers
+            for (Map.Entry<String, List<String>> e : serverKeyMap.entrySet()) {
+                ClientReply cr = readFromServer(e.getKey());
+                if (cr != null && cr.getStatus()) {
+                    if (protocolLOGGER.isLoggable(Level.SEVERE))
+                        logDifference(cr.getRotReply().getDsvItemList(), predictedDSV);
+                    updateDsv(cr.getRotReply().getDsvItemList());
+                    for (Map.Entry<Integer, Long> dti : cr.getRotReply().getDsItemsMap().entrySet()) {
+                        updateDS(dti.getKey(), dti.getValue());
+                    }
+                    results.putAll(cr.getRotReply().getKeyValueMap());
+                } else {
+                    protocolLOGGER.severe("Server could not get the keys= " + keys);
+                    return null;
+                }
             }
         } catch (Exception e) {
             protocolLOGGER.severe(Utils.exceptionLogMessge("Failed to get due to exception", e));
             return null;
         }
+        return results;
     }
-
 
     private int findPartition(String key) throws NoSuchAlgorithmException {
         long hash = Utils.getMd5HashLong(key);
@@ -146,5 +192,16 @@ public class CausalSpartanClient extends DKVFClient {
         for (int i = 0; i < dsv.size(); i++) {
             dsv.set(i, Math.max(dsv.get(i), dsvItemList.get(i)));
         }
+    }
+
+    /* Logs Mean Absolute Error (MAE) between actual and predicted vectors. */
+    private void logDifference(List<Long> actual, List<Long> prediction) {
+        long result = 0;
+
+        for (int i = 0; i < actual.size(); i++)
+            result += edu.msu.cse.orion.client.Utils.shiftToLowBits(Math.abs(actual.get(i) - prediction.get(i)));
+
+        protocolLOGGER.severe("DIFFERENCE " + (result + 0.0) / actual.size());
+
     }
 }
